60d1d8e43a22349bb8d3de0790acfee7b67b23b9
Anthony Lee <antwonlee@gmail.com>
2022-01-19 11:56:19 -0600

    feat: Add initial graph schema with ticket and buyer

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, JSONValue, json, ipfs } from "@graphprotocol/graph-ts";
 import {
   HFEvent,
   HFClaimEvent,
@@ -10,66 +10,32 @@ import {
   InvestorEarned,
   InvestsClaimed,
   PremiumReimbursed,
-  TicketStatusUpdate
-} from "../generated/HFEvent/HFEvent"
-import { ExampleEntity } from "../generated/schema"
+  TicketStatusUpdate,
+} from "../generated/HFEvent/HFEvent";
+import { Ticket, Buyer } from "../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
-export function handleHFClaimEvent(event: HFClaimEvent): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleTicketStatusUpdate(event: TicketStatusUpdate): void {
+  let buyerId = event.params.user.toHex();
+  let buyer = Buyer.load(buyerId);
+  if (buyer == null) {
+    buyer = new Buyer(buyerId);
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.ticketId = event.params.ticketId
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
+  let ticketId = event.address
+    .toHex()
+    .concat("-")
+    .concat(event.params.ticketId.toString());
+  let ticket = new Ticket(ticketId);
+  ticket.buyer = buyer.id;
+  ticket.ticketId = event.params.ticketId;
+  ticket.premiumAmount = event.params.premiumAmount;
+  ticket.totalInvestments = event.params.totalInvestments;
+  ticket.ticketName = event.params.ticketName;
+  ticket.previousStatus = event.params.previousStatus;
+  ticket.ticketStatus = event.params.ticketStatus;
+  ticket.payoutRatio = event.params.payoutRatio;
+  ticket.closingDate = event.params.closingDate;
+  ticket.startDate = event.params.startDate;
+  ticket.endDate = event.params.endDate;
+  ticket.save();
 }
-
-export function handleHFCoinBurned(event: HFCoinBurned): void {}
-
-export function handleHFCoinMinted(event: HFCoinMinted): void {}
-
-export function handleHFNFTMinted(event: HFNFTMinted): void {}
-
-export function handleInvestAdded(event: InvestAdded): void {}
-
-export function handleInvestReimbursed(event: InvestReimbursed): void {}
-
-export function handleInvestorEarned(event: InvestorEarned): void {}
-
-export function handleInvestsClaimed(event: InvestsClaimed): void {}
-
-export function handlePremiumReimbursed(event: PremiumReimbursed): void {}
-
-export function handleTicketStatusUpdate(event: TicketStatusUpdate): void {}
